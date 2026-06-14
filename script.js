dc09696ae8ce925978a4333c2ddeb6d342eaa40c
Exspieple <xx99593@gmail.com>
2026-06-14 17:54:21 +0200

    tiny styling improvements

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ import sitesList from "/sitesList.json" with { type: "json" };
 const sitesListWrapper = document.querySelector(".sites-list");
 
 sitesList.forEach((site) => {
-    sitesListWrapper.innerHTML += `
+  sitesListWrapper.innerHTML += `
         <li>
-          <a href="${site.href}" target="_blank">
-            <div class="site-button">
+          <a class="site-button" href="${site.href}" target="_blank">
               <img class="site-icon" src="/assets/${site.icon}" />
               <div class="site-title">${site.label}</div>
-            </div>
           </a>
         </li>
     `;
